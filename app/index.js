5d125df8e718cea755b0161fd5adade57f0605a2
chamikaVisal <chamikaperera456@gmail.com>
2020-08-24 04:21:35 +0530

    home ui partly done

---
+++ app/index.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native'
+import Animated, { Easing } from 'react-native-reanimated';
+import { TapGestureHandler, State } from 'react-native-gesture-handler';
+const { width, height } = Dimensions.get('window');
+
+const {
+    Value,
+    event,
+    block,
+    cond,
+    eq,
+    set,
+    Clock,
+    startClock,
+    stopClock,
+    debug,
+    timing,
+    clockRunning,
+    interpolate,
+    Extrapolate
+
+} = Animated
+
+function runTiming(clock, value, dest) {
+    const state = {
+        finished: new Value(0),
+        position: new Value(0),
+        time: new Value(0),
+        frameTime: new Value(0)
+    };
+
+    const config = {
+        duration: 1000,
+        toValue: new Value(0),
+        easing: Easing.inOut(Easing.ease)
+    };
+
+    return block([
+        cond(clockRunning(clock), 0, [
+            set(state.finished, 0),
+            set(state.time, 0),
+            set(state.position, value),
+            set(state.frameTime, 0),
+            set(config.toValue, dest),
+            startClock(clock)
+        ]),
+        timing(clock, state, config),
+        cond(state.finished, debug('stop clock', stopClock(clock))),
+        state.position
+    ]);
+}
+class MYSLT extends React.Component {
+    constructor() {
+        super()
+        this.buttonOpacity = new Value(1)
+        this.onStateChange = event([
+            {
+                nativeEvent: ({ state }) => block([
+                    cond(eq(state, State.END), set(this.buttonOpacity, runTiming(new Clock(), 1, 0)))
+                ])
+            }
+        ])
+        this.buttonY = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [100, 0],
+            extrapolate: Extrapolate.CLAMP
+        })
+        this.bgY = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [-height / 3, 0],
+            extrapolate: Extrapolate.CLAMP
+        })
+        this.logoY = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [height / 3, 0],
+            extrapolate: Extrapolate.CLAMP
+        })
+    }
+    render() {
+        return (
+            <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'flex-end' }}>
+
+                <Animated.View style={{ ...StyleSheet.absoluteFill, transform: [{ translateY: this.bgY }] }}>
+
+                    <Image
+                        source={require('../assets/bg.jpg')}
+                        style={{ flex: 1, height: null, width: null }}
+                    />
+
+                    <Animated.View style={{
+                        position: 'absolute', alignSelf: 'center', alignItems: 'center', alignSelf: 'center', marginTop: 80,
+                        transform: [{ translateY: this.logoY }]
+                    }}>
+                        <Image
+                            source={require('../assets/logo_slt.png')}
+                            style={{ position: 'absolute', }}
+                        />
+                    </Animated.View>
+
+                </Animated.View>
+
+                <View style={{ height: height / 3, justifyContent: 'center' }}>
+
+                    <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+                        <Animated.View style={{ ...styles.button, opacity: this.buttonOpacity, transform: [{ translateY: this.buttonY }] }}>
+                            <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#008ECC' }}>Register</Text>
+                        </Animated.View>
+                    </TapGestureHandler>
+
+                    <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+                        <Animated.View style={{ ...styles.button, backgroundColor: '#008ECC', borderWidth: 2, borderColor: 'white', opacity: this.buttonOpacity, transform: [{ translateY: this.buttonY }] }}>
+                            <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white' }}>Login</Text>
+                        </Animated.View>
+                    </TapGestureHandler>
+
+                </View>
+            </View>
+        )
+    }
+}
+export default MYSLT;
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: 'white',
+        height: 70,
+        marginHorizontal: 20,
+        borderRadius: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 5
+    }
+})
