f54b0b2b3baca6d49823e96cf43a66c9c5a1128f
chamikaVisal <chamikaperera456@gmail.com>
2020-09-28 04:38:16 +0530

    password change - ravindu added

--- app/AccountSettings.js
+++ app/AccountSettings.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-import { View, Image, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Animated, Text, Alert } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Animated, Text, Alert, Modal, TextInput } from 'react-native';
+import { Container, Header, Content, Button, Toast } from "native-base";
 
 
 class AccountSettings extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal1show: false,
+            modal2show: false
+        };
+    }
 
+    showToast = () => {
+        Toast.show({
+            text: "Password Successfully Reseted !",
+            position: 'bottom',
+            textStyle: { fontWeight: 'bold' },
+            duration: 4000,
+            style: { backgroundColor: "#00ff80", height: 80, margin: 15, borderRadius: 20, borderWidth: 1, borderColor: '#ffffff' },
+            buttonText: "Okay",
+            buttonStyle: { backgroundColor: "#ffffff", justifyContent: 'center', alignSelf: 'center' },
+            buttonTextStyle: { color: "#00ff80" },
 
+        })
+
+    }
 
     render() {
         return (
@@ -18,32 +39,160 @@ class AccountSettings extends Component {
 
                 {/* Create your profile UI's here -- Nishiki */}
                 <View style={{ flex: 1, backgroundColor: '#fffff' }}>
-                    
-                <View style={{ flex: 1, backgroundColor: '#ffffff' }}>
+
+                    <View style={{ flex: 1, backgroundColor: '#ffffff' }}>
                         <View style={{ justifyContent: "center", alignItems: 'center', marginTop: 60 }}>
                             <Text style={{ fontSize: 20, }}>Sri Lanka Telecom</Text>
                             <Text style={{ fontSize: 28, color: '#009eff' }}>Account Settings</Text>
                         </View>
-                        
-                       
-                        <View style={{flex:1,backgroundColor:'#fff', alignContent:'center'}}>
-                            <View style={{
-                            margin:15,flex:1,backgroundColor:'#ffffff',shadowColor: "#000",borderRadius:20, marginBottom: 90, backgroundColor: '#ebf8ff',
-                            shadowOffset: {
-                                width: 0,
-                                height: 2,
-                            },
-                            shadowOpacity: 0.25,
-                            shadowRadius: 3.84,
-                            
-                            elevation: 5,
-                            
-                        }}>
 
-                   
-    
+
+                        <View style={{ flex: 1, backgroundColor: '#fff', alignContent: 'center' }}>
+                            <View style={{
+                                margin: 15, flex: 1, backgroundColor: '#ffffff', shadowColor: "#000", borderRadius: 20, marginBottom: 90, backgroundColor: '#ebf8ff',
+                                shadowOffset: {
+                                    width: 0,
+                                    height: 2,
+                                },
+                                shadowOpacity: 0.25,
+                                shadowRadius: 3.84,
+
+                                elevation: 5,
+
+                            }}>
+                                {/* Ravindu's part */}
+                                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                    <TouchableOpacity onPress={() => { this.setState({ modal1show: true }) }}>
+                                        <Text style={{ color: '#009eff' }}> Change Broadband password</Text>
+                                    </TouchableOpacity>
+                                    {/* Ravindu's part */}
+                                </View>
+                                {/* MODAL change password--------THIS DOES NOT EFFECT YOUR CODE. YOU CAN JUST ASSUME THIS IS NOT A PART OF ACCOUNT SETTINGS */}
+
+                                <Modal style={{ flex: 1 }}
+                                    transparent={true}
+                                    visible={this.state.modal1show}>
+                                    <View style={{ backgroundColor: "#000000aa", flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+                                        <View style={{ backgroundColor: "#ffffff", paddingVertical: 30, borderTopColor: "black", paddingHorizontal: 30, borderRadius: 23, width: 340, }}>
+
+                                            <View style={{ flexDirection: 'row', }}>
+                                                <View style={{ flex: 10, alignItems: 'center', justifyContent: 'center' }}>
+                                                    <Text style={{ fontWeight: 'bold', fontSize: 18, color: '#222222' }}>Change password</Text>
+                                                </View>
+                                            </View>
+
+                                            <View
+                                                style={{
+                                                    borderBottomColor: '#d3d3d3',
+                                                    borderBottomWidth: 1,
+                                                    padding: 10
+                                                }}
+                                            ></View>
+                                            <View style={{ alignItems: 'center', alignContent: 'center', marginTop: 20, marginBottom: 10 }}>
+                                                <TextInput
+                                                    onFocus={this.onFocus}
+                                                    autoFocus={false}
+                                                    placeholder="                Enter current password                  "
+                                                    style={styles.textInput}
+                                                    placeholderTextColor="#C0C0C0"
+                                                    secureTextEntry={true}
+
+                                                />
+                                            </View>
+                                            <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+
+                                                <TouchableOpacity style={{ height: 45, width: 100, backgroundColor: '#ffffff', borderRadius: 23, justifyContent: 'center', alignItems: 'center', borderColor: 'silver', borderWidth: 1 }}
+                                                    onPress={() => { this.setState({ modal1show: false }) }}>
+                                                    <View style={{ justifyContent: 'center', alignItems: 'center', }}>
+                                                        <Text style={{ fontWeight: 'bold', color: 'silver' }}>Cancel</Text>
+                                                    </View>
+                                                </TouchableOpacity>
+
+                                                <TouchableOpacity style={{ height: 45, width: 100, backgroundColor: '#009eff', borderRadius: 23, justifyContent: 'center', alignItems: 'center', marginLeft: 10 }}
+                                                    onPress={() => { this.setState({ modal1show: false, modal2show: true }) }}>
+                                                    <View style={{ justifyContent: 'center', alignItems: 'center', }}>
+                                                        <Text style={{ fontWeight: 'bold', color: '#ffffff' }}>Next</Text>
+                                                    </View>
+                                                </TouchableOpacity>
+
+
+                                            </View>
+
+
+
+                                        </View>
+
+
+                                    </View>
+                                </Modal>
+                                {/* MODAL------------------------------------------------------------------------------------------------------------------- */}
+                                {/* MODAL new password--------THIS DOES NOT EFFECT YOUR CODE. YOU CAN JUST ASSUME THIS IS NOT A PART OF ACCOUNT SETTINGS */}
+
+                                <Modal style={{ flex: 1 }}
+                                    transparent={true}
+                                    visible={this.state.modal2show}>
+                                    <View style={{ backgroundColor: "#000000aa", flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+                                        <View style={{ backgroundColor: "#ffffff", paddingVertical: 30, borderTopColor: "black", paddingHorizontal: 30, borderRadius: 23, width: 340, }}>
+
+                                            <View style={{ flexDirection: 'row', }}>
+                                                <View style={{ flex: 10, alignItems: 'center', justifyContent: 'center' }}>
+                                                    <Text style={{ fontWeight: 'bold', fontSize: 18, color: '#222222' }}>Change password</Text>
+                                                </View>
+                                            </View>
+
+                                            <View
+                                                style={{
+                                                    borderBottomColor: '#d3d3d3',
+                                                    borderBottomWidth: 1,
+                                                    padding: 10
+                                                }}
+                                            ></View>
+                                            <View style={{ alignItems: 'center', alignContent: 'center', marginTop: 20, marginBottom: 10 }}>
+                                                <TextInput
+                                                    onFocus={this.onFocus}
+                                                    autoFocus={false}
+                                                    placeholder="               Enter new password                 "
+                                                    style={styles.textInput}
+                                                    placeholderTextColor="#C0C0C0"
+                                                    secureTextEntry={true}
+
+                                                />
+                                                <TextInput
+                                                    onFocus={this.onFocus}
+                                                    autoFocus={false}
+                                                    placeholder="            Re-enter new password              "
+                                                    style={styles.textInput}
+                                                    placeholderTextColor="#C0C0C0"
+                                                    secureTextEntry={true}
+
+                                                />
+                                            </View>
+                                            <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+
+
+                                                <TouchableOpacity style={{ height: 45, width: 100, backgroundColor: '#009eff', borderRadius: 23, justifyContent: 'center', alignItems: 'center', marginLeft: 10 }}
+                                                    onPress={() => { this.setState({ modal2show: false }, this.showToast) }}>
+                                                    <View style={{ justifyContent: 'center', alignItems: 'center', }}>
+                                                        <Text style={{ fontWeight: 'bold', color: '#ffffff' }}>Confirm</Text>
+                                                    </View>
+                                                </TouchableOpacity>
+
+
+                                            </View>
+
+
+
+                                        </View>
+
+
+                                    </View>
+                                </Modal>
+                                {/* MODAL------------------------------------------------------------------------------------------------------------------- */}
+
                             </View>
-                            </View>
+                        </View>
 
 
                     </View>
@@ -225,7 +374,17 @@ const styles = StyleSheet.create({
         borderColor: '#009eff'
 
 
-    }
+    },
+    textInput: {
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 0.5,
+
+        paddingLeft: 10,
+        marginVertical: 5,
+        borderColor: "#008ECC"
+
+    },
 
 
 });
